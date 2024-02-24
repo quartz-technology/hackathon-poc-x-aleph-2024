dcee74f4d30245e77134da5ea2f357baaabf4ad7
Vasek - Tom C <tom@quartz.technology>
2024-02-24 18:23:00 +0100

    feat(mirroring): support symlink
    Add `Symlink` struct.
    Add new unit tests.
    Support symbolink support in `Entry`.
    
    Signed-off-by: Vasek - Tom C <tom@quartz.technology>

--- src/mirroring/mod.rs
+++ src/mirroring/mod.rs
@@ -3,8 +3,10 @@ mod entry_type;
 mod entry;
 mod file;
 mod directory;
+mod sym_link;
 
 pub use entry_type::EntryType;
 pub use file::File;
 pub use directory::Directory;
 pub use fs_tree::FSTree;
+pub use sym_link::SymLink;
